2efd371e49a8e0abcb4ee639c10f15ff47b212df
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2025-02-24 20:46:31 +1100

    Added a switch to the circuit and applied logic to verify its functionality

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-# Turn an external LED on to check if the board
-# is functional and MicroPython was installed
-# correctly
+# Add a switch and if it is on the external led will
+# blink. Otherwise the led will be off
 
 from machine import Pin
 import time
 
 led = Pin(0, Pin.OUT)
+key = Pin(16, Pin.IN)
 
 while True:
-    led.toggle()
-    time.sleep(1)
+    if key.value():
+        led.toggle()
+        time.sleep(1)
+    else:
+        led.off()
