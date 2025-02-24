9e7730ab998a57fe2e4ae7d34a99d923fec2fb62
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2025-02-24 21:18:18 +1100

    Added more leds and switches

--- main.py
+++ main.py
@@ -4,12 +4,21 @@
 from machine import Pin
 import time
 
-led = Pin(0, Pin.OUT)
-key = Pin(16, Pin.IN)
+led1 = Pin(0, Pin.OUT)
+led2 = Pin(1, Pin.OUT)
+led3 = Pin(2, Pin.OUT)
+led4 = Pin(3, Pin.OUT)
+key1 = Pin(16, Pin.IN)
+key2 = Pin(17, Pin.IN)
+key3 = Pin(18, Pin.IN)
+key4 = Pin(19, Pin.IN)
+
+leds = [led1, led2, led3, led4]
+keys = [key1, key2, key3, key4]
 
 while True:
-    if key.value():
-        led.toggle()
-        time.sleep(1)
-    else:
-        led.off()
+    for index, key in enumerate(keys):
+        if key.value():
+            leds[index].on()
+        else:
+            leds[index].off()
