28b8f7629a1fdde178bab32c8c1b23790c9aa2e6
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2025-02-24 21:18:37 +1100

    Added/changed comments

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-# Add a switch and if it is on the external led will
-# blink. Otherwise the led will be off
+# Add a group of switches and if it is on a corresponding led will
+# turn on. Otherwise the led will be off
 
 from machine import Pin
-import time
 
 led1 = Pin(0, Pin.OUT)
 led2 = Pin(1, Pin.OUT)
@@ -13,9 +12,12 @@ key2 = Pin(17, Pin.IN)
 key3 = Pin(18, Pin.IN)
 key4 = Pin(19, Pin.IN)
 
+
+# Aggregate the leds and keys into lists
 leds = [led1, led2, led3, led4]
 keys = [key1, key2, key3, key4]
 
+# Loop through the keys and the corresponding leds on/off accordingly
 while True:
     for index, key in enumerate(keys):
         if key.value():
